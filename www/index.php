d8c6c8bbbc3d1ee8fb0e763a86876f119d519e3e
Bassim789 <bassim.matar@gmail.com>
2017-09-25 15:07:00 +0200

    -

--- www/index.php
+++ www/index.php
@@ -3,7 +3,7 @@ define('ROOT_PATH', realpath(__DIR__.'/../').'/');
 define('ROOT_PATH_PHP', ROOT_PATH.'php/');
 define('ROOT_PATH_WWW', ROOT_PATH.'www/');
 require_once ROOT_PATH_PHP.'util.php';
-require_once ROOT_PATH_PHP.'/lib/mustache/Autoloader.php';
+require_once ROOT_PATH_PHP.'/lib/Mustache/Autoloader.php';
 require_once ROOT_PATH_PHP.'loader.php';
 $config_file = file_get_contents(ROOT_PATH_WWW.'config.json');
 $config = json_decode($config_file, true);
